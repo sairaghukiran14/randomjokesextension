f0a6fa278393bb0a5ed8a83abdff0d59bb9aeda1
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-02-05 10:43:50 +0530

    3rd Commit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-fetch('https://dad-jokes.p.rapidapi.com/random/joke')
-.then(data=>data.json())
-.then(jokeData=>{
-    const jokeText = jokeData.body[0].setup;
-    const jokeElement = document.getElementById('jokeElement');
-    jokeElement.innerHTML = "A:  "+jokeText;
-    const jokeText1 = jokeData.body[0].punchline;
-    const jokeElement1 = document.getElementById('jokeElement');
-    jokeElement1.innerHTML = "B:  "+jokeText1;
-})
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+   $('body').css('color',color);
+}
